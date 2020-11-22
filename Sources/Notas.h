0ae2cb3db3f1ab5344d7011678439180f8f0c339
Archy <archyvan9092@gmail.com>
2020-11-22 15:19:47 +0800

    Project Init

---
+++ Sources/Notas.h
@@ -0,0 +1,18 @@
+//
+//  Notas.h
+//  Notas
+//
+//  Created by Archy on 2020/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Notas.
+FOUNDATION_EXPORT double NotasVersionNumber;
+
+//! Project version string for Notas.
+FOUNDATION_EXPORT const unsigned char NotasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Notas/PublicHeader.h>
+
+
